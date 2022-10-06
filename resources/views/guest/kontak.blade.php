917321beda63518d28e91e840b08b89f96ba55dc
Aditya <adityadr24@gmail.com>
2022-10-06 17:47:24 +0700

    kontak remove form

--- resources/views/guest/kontak.blade.php
+++ resources/views/guest/kontak.blade.php
@@ -27,7 +27,9 @@
                                 <p class="m-0 mt-15">Email: sman24jakartapusat@gmail.com <br>Telepon: 021 573-6984</p>
                             </div>
                         </div><!-- Item-->
-                        <div class="d-flex mt-50" data-show="startbox" data-show-delay="100">
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="d-flex" data-show="startbox" data-show-delay="100">
                             <div class="flex-shrink-0"><svg class="text-accent-1" xmlns="http://www.w3.org/2000/svg" width="15" height="17" fill="none">
                                     <path fill="currentColor" fill-rule="evenodd" d="M7.5 17S15 12.364 15 6.955c0-1.845-.79-3.614-2.197-4.918C11.397.733 9.49 0 7.5 0 5.51 0 3.603.733 2.197 2.037.79 3.34 0 5.11 0 6.955 0 12.364 7.5 17 7.5 17ZM10 7.286c0 1.341-1.12 2.428-2.5 2.428S5 8.627 5 7.286c0-1.342 1.12-2.429 2.5-2.429S10 5.944 10 7.286Z" clip-rule="evenodd" />
                                 </svg></div>
@@ -36,7 +38,9 @@
                                 <p class="m-0 mt-15">Jalan Lapangan Tembak Nomor 1, <br>Kel. Gelora, Kec. Tanah Abang <br/> Kota Jakarta Pusat 10270</p>
                             </div>
                         </div><!-- Item-->
-                        <div class="d-flex mt-50" data-show="startbox" data-show-delay="200">
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="d-flex" data-show="startbox" data-show-delay="200">
                             <div class="flex-shrink-0"><svg class="text-accent-1" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none">
                                     <path fill="currentColor" fill-rule="evenodd" d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0ZM8.7 3.379a1 1 0 1 0-2 0v4.8a1 1 0 0 0 .553.894l3.2 1.6a1 1 0 1 0 .894-1.789L8.7 7.561V3.379Z" clip-rule="evenodd" />
                                 </svg></div>
@@ -45,46 +49,6 @@
                                 <p class="m-0 mt-15">Senin - Jumat <br>08:00 - 15:00 WIB</p>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-lg-8" data-show="startbox" data-show-delay="300">
-                        <!-- Form-->
-                        <form>
-                            <div class="row gy-40">
-                                <div class="col-12 col-md-6"><input class="form-control" type="text" placeholder="Nama *"></div>
-                                <div class="col-12 col-md-6"><input class="form-control" type="email" placeholder="Email *"></div>
-                                <div class="col-12 col-md-6"><input class="form-control" type="tel" placeholder="Telepon *"></div>
-                                {{-- <div class="col-12 col-md-6"><input class="form-control" type="text" placeholder="Company"></div> --}}
-                                {{-- <div class="col-12 col-md-6"><select class="form-control">
-                                        <option selected="">I interested in ...</option>
-                                        <option>Colibri Creative Project</option>
-                                        <option>Colibri Project</option>
-                                        <option>Happy Moments Photoset</option>
-                                        <option>Luxury Car Website</option>
-                                        <option>Funny Dog</option>
-                                        <option>Creative Branding</option>
-                                    </select></div> --}}
-                                <div class="col-12 col-md-6"><select class="form-control">
-                                        <option selected="">Perihal *</option>
-                                        <option>Kurikulum</option>
-                                        <option>Kesiswaan</option>
-                                        <option>Sarana dan prasarana</option>
-                                        <option>Kerjasama</option>
-                                        <option>Administrator</option>
-                                    </select></div>
-                                <div class="col-12 mb-10"><textarea class="form-control" rows="2" placeholder="Message *"></textarea></div>
-                                {{-- <div class="col-12 col-md-6 col-lg-5">
-                                    <div class="form-attach-file"><label class="form-attach-label" for="formFile"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="26" fill="none">
-                                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.6" d="M24 12.251 13.126 22.953A7.162 7.162 0 0 1 8.103 25a7.162 7.162 0 0 1-5.022-2.047A6.935 6.935 0 0 1 1 18.009c0-1.854.748-3.632 2.08-4.943L13.955 2.365A4.775 4.775 0 0 1 17.303 1c1.256 0 2.46.491 3.348 1.365a4.624 4.624 0 0 1 1.387 3.295 4.624 4.624 0 0 1-1.387 3.296L9.766 19.657a2.387 2.387 0 0 1-1.675.683c-.627 0-1.23-.246-1.674-.683a2.312 2.312 0 0 1-.693-1.648c0-.618.25-1.21.693-1.647l10.046-9.875" />
-                                            </svg>Lampiran</label><input class="form-control" type="file" id="formFile" hidden></div>
-                                </div> --}}
-                                <div class="col-12 col-md-6 col-lg-4">
-                                    <div class="form-check"><input class="form-check-input" type="checkbox" value="" id="personalData"><label class="form-check-label" for="personalData">Saya setuju menggunakan data pribadi untuk diproses *</label></div>
-                                </div>
-                                <div class="col-12 col-lg-3 text-start text-lg-end">
-                                    <!-- Button--><button class="btn btn-secondary" disabled>Kirim</button>
-                                </div>
-                            </div>
-                        </form>
                     </div>
                 </div>
             </div>
