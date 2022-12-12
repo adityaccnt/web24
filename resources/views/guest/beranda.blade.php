0adfa61bfd3651bceeb701612e363d4f1a033563
adityadw0312 <aditya.dwi.rahmadi@unictive.com>
2022-12-12 20:57:55 +0700

    ig api

--- resources/views/guest/beranda.blade.php
+++ resources/views/guest/beranda.blade.php
@@ -417,5 +417,23 @@
         });
         feed.run();
     </script>
+    <script>
+        window.fbAsyncInit = function() {
+        FB.init({
+            appId      : '652373923296196',
+            xfbml      : true,
+            version    : 'v15.0'
+        });
+        FB.AppEvents.logPageView();
+        };
+
+        (function(d, s, id){
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) {return;}
+            js = d.createElement(s); js.id = id;
+            js.src = "https://connect.facebook.net/en_US/sdk.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+    </script>
 
 @endsection
