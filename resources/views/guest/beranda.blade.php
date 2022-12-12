6b8ea815b579b3a47cae24eab81c15b5f687a9df
adityadw0312 <aditya.dwi.rahmadi@unictive.com>
2022-12-12 20:59:37 +0700

    test facebook api

--- resources/views/guest/beranda.blade.php
+++ resources/views/guest/beranda.blade.php
@@ -409,6 +409,13 @@
         </div>
     </div>
 
+    <div
+  class="fb-like"
+  data-share="true"
+  data-width="450"
+  data-show-faces="true">
+</div>
+
     <script type="text/javascript">
         var feed = new Instafeed({
           accessToken: '{{ $token }}',
